e166e2be9be4a85930ff675c98557bc1db80e25d
우연 <bethelight730@gmail.com>
2025-07-25 21:56:38 +0900

    리팩터링 및 정리

--- GhostBall/Assets/02_Scripts/Card.cs
+++ GhostBall/Assets/02_Scripts/Card.cs
@@ -7,6 +7,4 @@ public class Card : MonoBehaviour
 
     public enum CardType { Red, Blue }
     public CardType Type;
-
-    // 카드별 추가 데이터 및 기능 필요시 여기에 작성
 } 
--- GhostBall/Assets/02_Scripts/TapRotateController.cs
+++ GhostBall/Assets/02_Scripts/TapRotateController.cs
@@ -8,7 +8,6 @@ public class TapRotateController : MonoBehaviour
     [SerializeField] private GameObject sparkEffect;
     [SerializeField] private GameObject orbEffect;
     [SerializeField] private GameObject otherCard;
-    [SerializeField] private GameObject popUp;
     private bool isRotating = false;
     private Vector2 tapStartPos;
     private float tapStartTime;
@@ -20,8 +19,6 @@ public class TapRotateController : MonoBehaviour
 
     private void Update()
     {
-        if (popUp != null && popUp.activeSelf)
-            return;
         if (isRotating)
             return;
         if (card != null && card.CurrentMotionState == Card.MotionState.DoubleTap)
